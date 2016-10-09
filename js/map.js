bbf6cb84560049b87815c2b9d182eb84632c8206
Jesus Vega <chuyspc@Jesuss-MacBook-Air.local>
2016-10-09 15:08:06 -0700

    Move file to correct directory

--- js/map.js
+++ js/map.js
@@ -5,6 +5,11 @@ function myMap() {
   //Locations
   var communityReginal = new google.maps.LatLng(52.395715,4.888916);
   var motivePower = new google.maps.LatLng(36.743099, -119.800406);
+  var bigPictureMiddle = new google.maps.LatLng(36.730273,-119.807915);
+  var chrisSorensonStudios = new google.maps.LatLng(36.719058,-119.778251);
+  var rooseveltMiddle = new google.maps.LatLng(36.742,-119.754);
+
+
 
   var mapCenter = new google.maps.LatLng(36.733577, -119.789057);
   var mapCanvas = document.getElementById("map");
@@ -31,5 +36,19 @@ function myMap() {
   var infowindow = new google.maps.InfoWindow({
   content: "<h1>Arthop Downtown</h1><p>Interactive Map</p>"
   });
+
+  // Polygon Construction
+  var polyCoords = [ motivePower , bigPictureMiddle , chrisSorensonStudios , rooseveltMiddle];
+  var cityPolygon = new google.maps.Polygon({
+    paths: polyCoords,
+    strokeColor: '#FF0000',
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: '#FF0000',
+    fillOpacity: 0.35
+  });
+  cityPolygon.setMap(map);
+
+
 infowindow.open(map,fultonMall);
 }
