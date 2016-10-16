db96ac2a50e7ba3392282b5209d6b5beff7bb171
Jesus Vega <chuyspc@Jesuss-MacBook-Air.local>
2016-10-15 19:41:55 -0700

    Integrate current location functionality

--- app/scripts/map.js
+++ app/scripts/map.js
@@ -1,14 +1,35 @@
-function myMap() {
-  //Corners
-  var topLeft = new google.maps.LatLng(36.743434, -119.801146);
+var coords;
 
-  //Polygon
-  /*
-      Top left 3 => 36.743129, -119.800229
-      Top right 1 > 36.742353, -119.803104
-      Top right corner 2 => 36.743600, -119.801833
-      Bottom left 36.728567, -119.787868
-  */
+function CenterControl(controlDiv, map) {
+
+      // Set CSS for the control border.
+      var controlUI = document.createElement('div');
+      controlUI.style.backgroundColor = '#fff';
+      controlUI.style.border = '2px solid #fff';
+      controlUI.style.borderRadius = '3px';
+      controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+      controlUI.style.cursor = 'pointer';
+      controlUI.style.marginBottom = '22px';
+      controlUI.style.textAlign = 'center';
+      controlUI.title = 'Click to recenter the map';
+      controlDiv.appendChild(controlUI);
+
+      // Set CSS for the control interior.
+      var controlText = document.createElement('div');
+      controlText.style.color = 'rgb(25,25,25)';
+      controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+      controlText.style.fontSize = '16px';
+      controlText.style.lineHeight = '38px';
+      controlText.style.paddingLeft = '5px';
+      controlText.style.paddingRight = '5px';
+      controlText.innerHTML = 'Center Map';
+      controlUI.appendChild(controlText);
+
+
+  }
+
+function myMap() {
+
 
   var poly1 = new google.maps.LatLng(36.743129, -119.800229);
   var poly2 = new google.maps.LatLng(36.742353, -119.803104);
@@ -19,28 +40,49 @@ function myMap() {
   var poly7 = new google.maps.LatLng(36.743337, -119.789315);
 
 
-
   var mapCenter = new google.maps.LatLng(36.733577, -119.789057);
   var mapCanvas = document.getElementById("map");
   var mapOptions = {center: mapCenter, zoom: 14};
   var map = new google.maps.Map(mapCanvas, mapOptions);
+
+  function currentLocation() {
+    if(navigator.geolocation){
+      function success(position) {
+        coords = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        }
+        map.panTo(coords);
+      }
+
+      function error() {
+        console.log("Location cannot be used/found");
+      }
+
+      var opt = {
+        enableHighAccuary: true,
+        timeout: 5000,
+        maximumAge: 0
+      };
+
+      navigator.geolocation.getCurrentPosition(success, error, opt);
+    }
+  }
+
   var fultonMall = new google.maps.Marker({
       position:mapCenter,
       icon: "images/mall.png",
       map:map
       });
-
-
-  var myCity = new google.maps.Circle({
-    center: topLeft,
-    radius: 25,
-    strokeColor: "#0000FF",
-    strokeOpacity: 1,
-    strokeWeight: 1,
-    fillColor: "#0000FF",
-    fillOpacity: 1
-  });
-  myCity.setMap(map);
+      var centerControlDiv = document.createElement('div');
+      var centerControl = new CenterControl(centerControlDiv, map);
+      centerControlDiv.index = 1;
+      map.controls[google.maps.ControlPosition.BOTTOM_CENTER].push(centerControlDiv);
+      // Setup the click event listeners: simply set the map to Chicago.
+      centerControlDiv.addEventListener('click', function() {
+        currentLocation();
+        console.log(coords);
+      });
 
   var infowindow = new google.maps.InfoWindow({
   content: "<h1>Arthop Downtown</h1><p>Interactive Map</p>"
