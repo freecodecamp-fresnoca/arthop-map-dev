aa93663e2bcd57c95550ca9d09bc9f58e06511a0
Rawradin <edgar.rawr@gmail.com>
2016-10-21 23:28:00 -0700

    remove config from git history and add to gitignore

--- app/scripts/config.js
+++
@@ -1,8 +0,0 @@
-var mapAPI = "API KEY GOES HERE";
-
-//Add your api key on the mapAPI variable above
-var apiScript = document.createElement('script');
-apiScript.setAttribute('src','https://maps.googleapis.com/maps/api/js?key='+mapAPI + "&callback=myMap");
-var lastElement = document.body.lastChild;
-
-lastElement.appendChild(apiScript);
