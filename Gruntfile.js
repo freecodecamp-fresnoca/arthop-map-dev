5fea9a015752677f5d031324818060cc1a759847
Van <vathana.to@outlook.com>
2016-10-27 03:44:04 +0000

    Fix Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -72,7 +72,7 @@ module.exports = function (grunt) {
       options: {
         port: 8080,
         // Change this to '0.0.0.0' to use it with CLOUD9 and to access outside.
-        hostname: '0.0.0.0',
+        hostname: 'localhost',
         livereload: 35729
       },
       livereload: {
