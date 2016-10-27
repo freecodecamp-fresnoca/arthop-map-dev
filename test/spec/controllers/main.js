497411c75d02c4d48a069b0d6eb0800ded38685b
Michael Chrisco <michaelc@cvtrust.org>
2016-10-27 16:52:24 -0700

    Include grunt-karma in repo.   - Remove testing until proper tests are created.

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -8,16 +8,16 @@ describe('Controller: MainCtrl', function () {
   var MainCtrl,
     scope;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
-      $scope: scope
-      // place here mocked dependencies
-    });
-  }));
-
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(MainCtrl.awesomeThings.length).toBe(3);
-  });
+  // // Initialize the controller and a mock scope
+  // beforeEach(inject(function ($controller, $rootScope) {
+  //   scope = $rootScope.$new();
+  //   MainCtrl = $controller('MainCtrl', {
+  //     $scope: scope
+  //     // place here mocked dependencies
+  //   });
+  // }));
+  //
+  // it('should attach a list of awesomeThings to the scope', function () {
+  //   expect(MainCtrl.awesomeThings.length).toBe(3);
+  // });
 });
