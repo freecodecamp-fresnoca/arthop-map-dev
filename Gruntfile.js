cb576b4f60266245f7b1aaddb982a835b4ea55cf
Van <vathana.to@outlook.com>
2016-10-27 03:05:33 +0000

    Add checkmark in search functionality

--- Gruntfile.js
+++ Gruntfile.js
@@ -72,7 +72,7 @@ module.exports = function (grunt) {
       options: {
         port: 8080,
         // Change this to '0.0.0.0' to use it with CLOUD9 and to access outside.
-        hostname: 'localhost',
+        hostname: '0.0.0.0',
         livereload: 35729
       },
       livereload: {
