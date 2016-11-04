4a3cc12989852d5fb162bf19f9eb5f7a6f755c37
Rawradin <edgar.rawr@gmail.com>
2016-11-03 20:39:43 -0700

    setup venues endpoint to serve venues json data

--- app/backend/app.js
+++ app/backend/app.js
@@ -3,12 +3,15 @@ let express        = require('express'),
     bodyParser     = require("body-parser"),
     mongoose       = require("mongoose"),
     methodOverride = require("method-override")
-    morgan         = require("morgan")
+    morgan         = require("morgan"),
+    venues         = require('./routes/venues.js')
 
 mongoose.connect("localhost:27017") 
 app.use(bodyParser.urlencoded({extended: true})) 
 app.use(methodOverride('_method')) 
 app.use(morgan('dev')) 
+
+app.use('/venues',venues);
 
 let server = app.listen(process.env.PORT || 3000, process.env.IP, function(){
      console.log("ArtHop backend server started.")
