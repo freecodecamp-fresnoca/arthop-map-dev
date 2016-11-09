89ed2c98ae278ddda645136d8dea3c45a964243a
Rawradin <edgar.rawr@gmail.com>
2016-11-09 00:42:39 -0800

    google oauth working. was missing passport serializer and deserializer as well as express-sessions

--- app/backend/app.js
+++ app/backend/app.js
@@ -6,54 +6,82 @@ const express        = require('express'),
     morgan         = require("morgan"),
     venues         = require('./routes/venues.js'),
     googleAuth     = require('./config/googleAuth.js'),
-    passport       = require('passport')
-    GoogleStrategy = require('passport-google-oauth20').Strategy,
-    User           = require('./models/user.js')
+    passport       = require('passport'),
+    User           = require('./models/user.js'),
+    GoogleStrategy = require('passport-google-oauth').OAuth2Strategy,
+    cookieParser   = require('cookie-parser'),
+    session        = require('express-session')
 
 mongoose.connect("localhost:27017") 
-app.use(bodyParser.urlencoded({extended: true})) 
+app.use(bodyParser.json()) 
 app.use(methodOverride('_method')) 
 app.use(morgan('dev')) 
+app.use(cookieParser());
+app.use(session({ 
+  secret: '#jessie #teamdonut #triggered' ,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true  }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
 
 passport.use(new GoogleStrategy({
-    clientID: googleAuth.clientID,
-    clientSecret: googleAuth.clientSecret,
-    callbackURL: googleAuth.callbackURL
+    clientID        : googleAuth.clientID,
+    clientSecret    : googleAuth.clientSecret,
+    callbackURL     : googleAuth.callbackURL,
   },
-  function (accessToken, refreshToken, profile, cb) {
-    User.find({email: profile.email}, (err, user) => {
-      if(err) {
-        console.log("Error", err)
-      } else {
-        if(user.length !== 0) {
-          console.log("Found", user)
-          return cb(err, user);
+  function(token, refreshToken, profile, done) {
+    // make the code asynchronous
+    // User.findOne won't fire until we have all our data back from Google
+    process.nextTick(function() {
+      // try to find the user based on their google id
+      User.findOne({ 'google.id' : profile.id }, function(err, user) {
+        if (err)
+          return done(err);
+        if (user) {
+          // if a user is found, log them in
+          return done(null, user);
         } else {
-          let newUser = User({
-            username: profile.displayName, 
-            email: profile.emails[0].value, 
-            photoURL: profile.photos[0].value,
-            googleId: profile.id,
-            points: 0,
-            locations: []
-          })
-          newUser.save((err) => {
-            if (err) throw err;
-            console.log("USER CREATED")
-            return cb(err,user)
-          })  
+          // if the user isnt in our database, create a new user
+          var newUser          = new User();
+          // set all of the relevant information
+          newUser.id    = profile.id;
+          newUser.token = token;
+          newUser.username  = profile.displayName;
+          newUser.email = profile.emails[0].value; // pull the first email
+          // save the user
+          newUser.save(function(err) {
+            if (err)
+                throw err;
+            return done(null, newUser);
+          });
         }
-      }
-    })
-  }
-));
+      });
+    });
+  })
+);
 
 app.get('/', (req,res) => {res.send("<a href='/auth/google'>Google login</a>")})
 app.get('/auth/google', passport.authenticate('google', { scope: ['email profile'] }))
+
 app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), function(req, res) {
   // Successful authentication, redirect home.
-  console.log(req, " || ", res)
+  console.log('\n\n\n\n\n\n\n',res,'\n\n\n\n\n\n')
   res.send('Success')
+})
+
+app.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
 })
 
 app.use('/venues',venues);
