5abdb1e2490345e3e3ada26995681d89626b3713
Rawradin <edgar.rawr@gmail.com>
2016-11-10 20:02:10 -0800

    google oAuth working with Bearer JWT

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ const express      = require('express'),
     app            = express(),
     bodyParser     = require("body-parser"),
     mongoose       = require("mongoose"),
-    methodOverride = require("method-override")
+    methodOverride = require("method-override"),
     morgan         = require("morgan"),
     venues         = require('./backend/routes/venues.js'),
     googleAuth     = require('./backend/config/googleAuth.js'),
@@ -11,7 +11,8 @@ const express      = require('express'),
     GoogleStrategy = require('passport-google-oauth').OAuth2Strategy,
     cookieParser   = require('cookie-parser'),
     session        = require('express-session'),
-    path           = require('path')
+    path           = require('path'),
+    BearerStrategy = require('passport-http-bearer').Strategy
 
 mongoose.connect("localhost:27017") 
 
@@ -19,80 +20,80 @@ app.use('/', express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.json()) 
 app.use(methodOverride('_method')) 
 app.use(morgan('dev')) 
-app.use(cookieParser())
-app.use(session({ 
-  secret: '#jessie #teamdonut #triggered' ,
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: true  }
-}))
-app.use(passport.initialize())
-app.use(passport.session())
-
-passport.serializeUser(function(user, done) {
-  done(null, user)
-})
-
-passport.deserializeUser(function(user, done) {
-  done(null, user)
-})
 
 passport.use(new GoogleStrategy({
     clientID        : googleAuth.clientID,
     clientSecret    : googleAuth.clientSecret,
     callbackURL     : googleAuth.callbackURL,
   },
-  function(token, refreshToken, profile, done) {
-    // make the code asynchronous
-    // User.findOne won't fire until we have all our data back from Google
-    process.nextTick(function() {
-      // try to find the user based on their google id
-      User.findOne({ 'googleId' : profile.id }, function(err, user) {
-        if (err)
-          return done(err);
-        if (user) {
-          // if a user is found, log them in
-          console.log("\n\n\nUser already exists\n\n\n")
-          return done(null, user);
-        } else {
-          console.log("\n\n\nSaving new user to database\n\n\n")
-          // if the user isnt in our database, create a new user
-          var newUser          = new User();
-          // set all of the relevant information
-          newUser.googleId    = profile.id;
-          newUser.googleToken = token;
-          newUser.username  = profile.displayName;
-          newUser.email = profile.emails[0].value; // pull the first email
-          newUser.points = 0
-          newUser.locations = ["sample","stuff"]
-          // save the user
-          newUser.save(function(err) {
-            if (err)
-                throw err;
-            return done(null, newUser);
-          })
+  function(accessToken, refreshToken, profile, done) {
+    User.findOrCreate(
+      { googleId: profile.id },
+      profile,
+      function (err, result) {
+          if(result) {
+            result.access_token = accessToken;
+            result.save(function(err, doc) {
+              done(err, doc);
+            });
+          } else {
+            done(err, result);
+          }
         }
-      })
-    })
-  })
+      );
+    }
+  )
 )
+
+passport.use(
+    new BearerStrategy(
+        function(token, done) {
+            User.findOne({ access_token: token },
+                function(err, user) {
+                    if(err) {
+                        return done(err)
+                    }
+                    if(!user) {
+                        return done(null, false)
+                    }
+
+                    return done(null, user, { scope: 'all' })
+                }
+            );
+        }
+    )
+);
+
 
 app.get('/auth/google', passport.authenticate('google', { scope: ['email profile'] }))
 
-app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), function(req, res) {
-  // Successful authentication, redirect home.
-  res.send("Success<a href='/auth/google'>Google login</a>")
-})
+app.get('/auth/google/callback',
+  passport.authenticate('google', { session: false, failureRedirect: "/" }),
+  function(req, res) {
+    res.redirect("/profile?access_token=" + req.user.access_token);
+  }
+)
+
+app.get(
+    '/profile',
+    passport.authenticate('bearer', { session: false }),
+    function(req, res) {
+      res.redirect('/')
+        // res.send("LOGGED IN as " + req.user.toString() + " - <a href=\"/logout\">Log out</a>");
+    }
+);
 
 app.get('/logout', function(req, res) {
   req.logout()
   res.redirect('/')
 })
 
-app.use('/venues',venues)
-//app.use('/', function(req, res) {
- // res.sendFile(path.join(__dirname + '/index.html'))
-//})
+app.get('/test', function(req, res) {
+  var sess = req.session
+  console.log(sess,"\n\n\nSESSSSSSION")
+})
+
+app.use('/venues', venues)
 
 let server = app.listen(3333, function(){
    console.log("ArtHop backend server started.")
--- app/backend/models/user.js
+++ app/backend/models/user.js
@@ -5,7 +5,29 @@ var userSchema = new mongoose.Schema({
     email: String,
     imageURL: String,
     points: Number,
-    googleId: Number
+    googleId: Number,
+    access_token: { type: String }
 });
+
+userSchema.statics.findOrCreate = function(filters, profile, cb) {
+    User = this;
+    this.find(filters, function(err, results) {
+      if(results.length == 0) {
+        var newUser = new User()
+        newUser.googleId = filters.googleId
+        newUser.username  = profile.displayName
+        newUser.email = profile.emails[0].value // pull the first email
+        newUser.photoURL = profile.photos[0].value
+        newUser.points = 0
+        // newUser.locations = ["sample","stuff"]
+
+        newUser.save(function(err, doc) {
+          cb(err, doc)
+        });
+      } else {
+        cb(err, results[0]);
+      }
+    });
+  };
 
 module.exports = mongoose.model("User", userSchema);
