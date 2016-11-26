c99922c6ec5768bfbb099e41cfcc3582bbd8d29f
Rawradin <edgar.rawr@gmail.com>
2016-11-25 20:47:14 -0800

    user is now able to get a point for correctly entered businessKey

--- app/backend/routes/venues.js
+++ app/backend/routes/venues.js
@@ -1,6 +1,9 @@
 //venues endpoint
 const venues  = require('../models/venue.js'),
+      User    = require('../models/user.js'),
       express = require('express'),
+      jwt     = require('jwt-simple'),
+      moment  = require('moment'),
       router  = express.Router()
 
 router.get('/', (req,res,next) => {
@@ -10,20 +13,59 @@ router.get('/', (req,res,next) => {
   res.send(output)
 })
 
-router.post('/verify', (req, res, next) => {
+router.post('/verify',ensureAuthenticated, (req, res, next) => {
   // console.log(req.body);
-  /* req.body ideal look
+  /* req.body
   { 
     name: 'Bitwise Industries',
-    location: { lat: 123, lng: 101  },
     business_key: 'SAMPLE KEY PHASE'  
   }
   */
-  let inputKey = req.body.businessKey
-  let venue = venues().find(v => {
-    return v.name === req.body.name;
-  }) 
-  res.send(venue)
+  let inputKey = req.body.key
+  let venue = venues().find(v => { return v.name === req.body.name; }) 
+
+  // TODO: I will also need to check if user has already entered code for the venue
+  if(venue.businessKey === inputKey) {
+    User.findById(req.user, function(err, user) {
+      
+      user.points += 1 //We will want to do something like venue.points if we implement different points for each venue
+      user.save(function(err) {
+        if(err) {
+          console.log("Error with saving the user", err)
+          res.send({text: "Sorry error with saving your points"})
+        }
+        console.log("Points added to", user.displayName)
+        res.send({ text: "Congrats! Here's a cookie.", points: user.points })
+      })
+    })
+  } else {
+    res.send({text: "Sorry key doesn't match."})
+  }
 })
 
 module.exports = router
+
+function ensureAuthenticated(req, res, next) {
+  //I will have to refactor all the middleware/authentication into its own file
+  const TOKEN_SECRET = "secrit secrit"
+  if (!req.header('Authorization')) {
+    return res.status(401).send({ message: 'Please make sure your request has an Authorization header' });
+  }
+  var token = req.header('Authorization').split(' ')[1];
+
+  var payload = null;
+  try {
+    payload = jwt.decode(token, TOKEN_SECRET);
+  }
+  catch (err) {
+    return res.status(401).send({ message: err.message });
+  }
+
+  if (payload.exp <= moment().unix()) {
+    return res.status(401).send({ message: 'Token has expired' });
+  }
+  req.user = payload.sub;
+  next();
+}
+
+
