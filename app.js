3f4a124f0f3f487a973ff250472fa01a59d7c3f6
Rawradin <edgar.rawr@gmail.com>
2016-12-01 18:10:35 -0800

    add missing line of code to fix static serving

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const express      = require('express'),
 let db = process.env.DB_CONN || "localhost:27017"
 
 mongoose.connect(db) 
-
+process.env.PWD = process.cwd()
 app.use('/', express.static(path.join(process.env.PWD + '/dist')))
 app.use(bodyParser.json()) 
 app.use(bodyParser.urlencoded({ extended: true  }))
