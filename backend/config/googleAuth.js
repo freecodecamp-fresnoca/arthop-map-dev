609d41aa1b614799901ee1fcbce4f44708820307
Rawradin <edgar.rawr@gmail.com>
2016-12-05 18:49:42 -0800

    make dev environment working again

--- backend/config/googleAuth.js
+++ backend/config/googleAuth.js
@@ -1,6 +1,7 @@
 let secrets = {
   clientID: process.env.clientID,
   clientSecret: process.env.clientSecret,
-  callbackURL: "https://arthop.herokuapp.com" || 'http://localhost:3333'
+  callbackURL: process.env.callbackURL || 'http://localhost:3333'
 }
 module.exports = secrets
+
