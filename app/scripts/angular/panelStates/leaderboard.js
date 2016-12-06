e16a91051de50a3046cb7d957bdeb9f89585d117
Jesus Vega <chuyspc@Jesuss-MacBook-Air.local>
2016-12-05 20:19:26 -0800

    Finish Leaderboard

--- app/scripts/angular/panelStates/leaderboard.js
+++ app/scripts/angular/panelStates/leaderboard.js
@@ -1,45 +1,10 @@
-function LeaderboardController() {
+function LeaderboardController($http) {
   var ctrl = this;
 
-  /*
-    TODO:
-    Sort from highest to lowest
-  */
-
-   this.userslist = [{
-        "displayName" : "Moist FlipFlop",
-        "score" : 2
-    },
-    {
-        "displayName" : "Not Cheating",
-        "score" : 4
-    },
-    {
-        "displayName" : "Greatest Ever",
-        "score" : 5
-    },
-    {
-        "displayName" : "Steve Jobs",
-        "score" : 10
-    },
-    {
-        "displayName" : "Steve Wozniak",
-        "score" : 12
-    },
-    {
-        "displayName" : "John Ives",
-        "score" : 11
-    },
-    {
-          "displayName" : "Independant Port",
-          "score" : 18
-    },
-    {
-          "displayName" : "Lukewarm Lasagna",
-          "score" : 22
-    }
-  ];
-
+  $http.get('/users')
+  .then(function(response){
+    ctrl.users = response.data.users;
+  });
 }
 
 angular.module('arthopMapApp').controller('LeaderboardController', LeaderboardController);
